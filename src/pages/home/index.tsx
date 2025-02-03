6352d6614fff8b251838cfe65b3ab509173f95f8
afonin.vladimir <vova.afonin.2001@mail.ru>
2025-02-03 23:44:54 +0400

    Commit #3

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,12 @@
+import {PersonageCard} from "@/components/personage-card.tsx";
 
-export default function IndexHome(){
+export default function IndexHome() {
 
-    return(
-        <>Home</>
+    return (
+        <div className={"grid grid-flow-col grid-rows-4 gap-4"}>
+            <PersonageCard></PersonageCard>
+            <PersonageCard></PersonageCard>
+            <PersonageCard></PersonageCard>
+        </div>
     )
 }
