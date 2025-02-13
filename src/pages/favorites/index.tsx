579dd99675e37d04567349722b86673de6fd1782
afonin.vladimir <vova.afonin.2001@mail.ru>
2025-02-13 21:29:23 +0400

    Commit #8 Страница с избранными

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,7 +1,45 @@
+import {PersonageCard} from "@/components/personage-card.tsx";
+import {useState, useEffect} from "react";
+import axiosInstance from "@/instances/axios_instance.tsx";
+import {Character} from "@/types/Character.tsx";
+import "@/instances/localStorage_instance.tsx"
+import {localStorageGet, localStorageSet} from "@/instances/localStorage_instance.tsx";
 
-export default function IndexFavorites(){
+export default function IndexFavorites() {
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const [favorites, setFavorites] = useState<number[]>(() => {
+        const localFavorites = localStorageGet<number[]>("favorites");
+        return localFavorites !== null ? localFavorites : [];
+    });
 
-    return(
-        <>Favorites</>
+
+
+
+    useEffect(() => {
+        const sendRequest = async () => {
+            await axiosInstance.get(`/character/${favorites}`)
+                .then(response => {
+                    setCharacters(response.data ?? [])
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+
+        }
+        sendRequest();
+    },[favorites])
+
+    useEffect(() => {
+        localStorageSet<number[]>("favorites", favorites);
+    }, [favorites]);
+
+    return (
+        <>
+            <div className={"grid grid-cols-4 gap-4 mt-8"}>
+                {characters.map((character) => (
+                    <PersonageCard personage={character} favorites={favorites} setFavorites={setFavorites}></PersonageCard>
+                ))}
+            </div>
+        </>
     )
 }
