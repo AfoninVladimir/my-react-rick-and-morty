47b4a60a1dabf9ec56a01caf479ed343b0c07d01
afonin.vladimir <vova.afonin.2001@mail.ru>
2025-02-13 23:04:56 +0400

    Commit #10

--- src/pages/personage/index.tsx
+++ src/pages/personage/index.tsx
@@ -1,55 +1,53 @@
 import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@/components/ui/card.tsx";
 import {PersonageStatus} from "@/enums/PersonageStatus.tsx";
 import {Heart, HelpCircle, Skull} from "lucide-react";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import axiosInstance from "@/instances/axios_instance.tsx";
 import {Character} from "@/types/Character.tsx";
 
-export default function IndexPersonage({personageId}: {personageId: number}) {
+export default function IndexPersonage({personageId}: { personageId: number }) {
     const [personage, setPersonage] = useState<Character>();
-    useEffect(() => {
-        const sendRequest = async () => {
-            await axiosInstance.get(`/character/${personageId}`)
-                .then(response => {
-                    setPersonage(() => response.data);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
+    const sendRequest = async () => {
+        await axiosInstance.get(`/character/${personageId}`)
+            .then(response => {
+                setPersonage(() => response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            })
 
-        }
-        sendRequest();
-    },[])
+    }
+    sendRequest();
     console.log(personage)
-    return(
+    return (
         <>
             {personage ? (
-            <Card className={"max-w-sm min-w-96"}>
-                <CardHeader>
-                    <img src={personage.image} alt={personage.name} className={"border rounded-xl"}/>
-                    <CardTitle className={"text-center"}>{personage.name}</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <CardDescription className={"text-lg"}>
-                        <div className={"flex"}>
-                            <p>Status:</p>
-                            {personage.status === PersonageStatus.Alive && <Heart/>}
-                            {personage.status === PersonageStatus.Dead && <Skull/>}
-                            {personage.status === PersonageStatus.Unknown && <HelpCircle/>}
-                        </div>
-                        <div>Species: {personage.species}</div>
-                        {personage.type && <div>Type: {personage.type}</div>}
-                        <div>Gender: {personage.gender}</div>
-                        <div>Origin: {personage.origin.name}</div>
-                        <div>Location: {personage.location.name}</div>
-                        <div>Gender: {personage.gender}</div>
-                        <div>Episodes: [{personage.episode.map((item) => (item.split("/").pop())).join(", ")}]</div>
-                    </CardDescription>
-                </CardContent>
-                <CardFooter>
-                </CardFooter>
-            </Card>
-                ) : (
+                <Card className={"max-w-sm min-w-96"}>
+                    <CardHeader>
+                        <img src={personage.image} alt={personage.name} className={"border rounded-xl"}/>
+                        <CardTitle className={"text-center"}>{personage.name}</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <CardDescription className={"text-lg"}>
+                            <div className={"flex"}>
+                                <p>Status:</p>
+                                {personage.status === PersonageStatus.Alive && <Heart/>}
+                                {personage.status === PersonageStatus.Dead && <Skull/>}
+                                {personage.status === PersonageStatus.Unknown && <HelpCircle/>}
+                            </div>
+                            <div>Species: {personage.species}</div>
+                            {personage.type && <div>Type: {personage.type}</div>}
+                            <div>Gender: {personage.gender}</div>
+                            <div>Origin: {personage.origin.name}</div>
+                            <div>Location: {personage.location.name}</div>
+                            <div>Gender: {personage.gender}</div>
+                            <div>Episodes: [{personage.episode.map((item) => (item.split("/").pop())).join(", ")}]</div>
+                        </CardDescription>
+                    </CardContent>
+                    <CardFooter>
+                    </CardFooter>
+                </Card>
+            ) : (
                 <p>Loading...</p>
             )}
         </>
